aee815f4c30f99431dcf70dece5038dd4c300031
Hari Krishnan <hariraghupathy@gmail.com>
2024-05-05 01:08:20 +0530

    [Hari] - added private contract package and setup initial loading

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import cors, { CorsOptions } from "cors";
 import express, { Express } from "express";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
+import spec from "@hk-boilerplate/demyst-todo-spec/dist/spec";
+import YAML from "yamljs";
+import { summarise } from "swagger-routes-express";
 
 export async function createServer(): Promise<Express> {
-
   // cors config
   const corsOption: CorsOptions = {
     origin: [/http:\/\/localhost:*/],
@@ -12,6 +14,14 @@ export async function createServer(): Promise<Express> {
   };
 
   // reading yaml file from private npm package - api contract
+  const yamlSpecFile = require("path").resolve(
+    __dirname,
+    "../node_modules/@hk-boilerplate/demyst-todo-spec/dist/spec.yaml"
+  );
+  const apiDef = YAML.load(yamlSpecFile);
+  console.log(apiDef);
+  const apiSummary = summarise(spec);
+  console.info(apiSummary);
 
   const server = await express();
 
