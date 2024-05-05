12fd11ab2a090af05c7bd3dd7ab895587c03b083
Hari Krishnan <hariraghupathy@gmail.com>
2024-05-05 10:27:29 +0530

    [Hari] - added jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/src/app.ts",   
+    "/src/index.ts",
+    "/build"
+  ],
 };
